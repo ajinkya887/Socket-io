c46f9b8867c81104cc32c68b4c7a7c6bf85d06e3
ajinkya887 <ajinkyasontakke07@gmail.com>
2025-04-24 12:20:17 +0530

    added env

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,1 +1,3 @@
-export const APIUrl = "http://localhost:3000";
+// export const APIUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
+export const APIUrl = "https://socket-io-backend-h2zj.onrender.com";
